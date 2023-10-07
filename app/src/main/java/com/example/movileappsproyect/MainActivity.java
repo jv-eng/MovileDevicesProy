9505993990b0dd29483bbcc8df4f5d2ad939a9de
jv-eng <combrogi.viejo@gmail.com>
2023-10-07 13:34:25 +0200

    Inicio

--- app/src/main/java/com/example/movileappsproyect/MainActivity.java
+++ app/src/main/java/com/example/movileappsproyect/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final static String TAG = MainActivity.class.getName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
