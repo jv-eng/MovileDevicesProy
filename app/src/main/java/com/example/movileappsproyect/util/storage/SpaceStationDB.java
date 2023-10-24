00f03539517a7680d900f04dba4f8ae2562658c4
jv-eng <combrogi.viejo@gmail.com>
2023-10-24 19:05:00 +0200

    Create SpaceStationDB.java

---
+++ app/src/main/java/com/example/movileappsproyect/util/storage/SpaceStationDB.java
@@ -0,0 +1,19 @@
+package com.example.movileappsproyect.util.storage;
+
+import com.example.movileappsproyect.model.SpaceStationModel;
+
+import java.util.List;
+
+public class SpaceStationDB {
+    private SpaceStationHelper helper;
+    public SpaceStationDB(SpaceStationHelper helper) {
+        this.helper = helper;
+    }
+
+    public List<SpaceStationModel> getAll(){
+        return null;
+    }
+    public void save(SpaceStationModel station) {
+
+    }
+}
